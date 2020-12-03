59b325d4a1380d5de9861216901fe7fb834742f4
marmelo7 <74640788+marmelo7@users.noreply.github.com>
2020-12-03 21:59:03 +0900

    Add files via upload
    markup

---
+++ todo/_.js
@@ -0,0 +1,8 @@
+const input = document.querySelector("#input");
+const container = input.parentNode.parentNode;
+
+input.addEventListener("keydown", function(e){
+    if (e.keyCode == 13){
+        input.parentNode.parentNode.append
+    }
+});
