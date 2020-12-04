91f66863311128b0e98d2137d92210a3f5d560f9
marmelo7 <74640788+marmelo7@users.noreply.github.com>
2020-12-04 12:56:40 +0900

    Add files via upload

--- todo/_.js
+++ todo/_.js
@@ -1,8 +1,47 @@
 const input = document.querySelector("#input");
-const container = input.parentNode.parentNode;
+const con = document.querySelector("#container");
+const add = document.querySelector("#add");
+const checkIcon = document.querySelector("#check")
+const deleteIcon = document.querySelector("#delete")
+
+function makeCheck(){
+    const newCheck = document.createElement("button");
+    newCheck.setAttribute("id", "check");
+    con.appendChild(newCheck);
+}
+
+function makeList(){
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML = input.value;
+    input.value = "";
+    newDiv.setAttribute("id", "list");
+    con.appendChild(newDiv);
+}
+
+function makeDelete(){
+    const newDelete = document.createElement("button");
+    newDelete.setAttribute("id", "delete");
+    con.appendChild(newDelete);
+}
+
+function makeTodo (){
+    makeCheck();
+    makeDelete();
+    makeList();
+}
 
 input.addEventListener("keydown", function(e){
-    if (e.keyCode == 13){
-        input.parentNode.parentNode.append
+    if (input.value !== "" && e.keyCode == 13){
+        makeTodo();
     }
 });
+
+add.addEventListener("click", function(){
+    if (input.value !== ""){
+        makeTodo();
+    }
+});
+
+checkIcon.addEventListener("click", function(){
+
+})
